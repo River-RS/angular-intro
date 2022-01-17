4fdb4c0302738049a0327ac9f33bca659b6ff86e
River <jSTEIN99@nmsu.edu>
2022-01-16 19:39:55 -0700

    feat: tasks component, task interface, mock tasks

---
+++ files/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Tasks';
+import { TASKS } from '../../mock-tasks';
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
